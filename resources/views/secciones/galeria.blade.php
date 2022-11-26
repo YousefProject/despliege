22b73680b9e81f388ab192c0ece9a18d837d3310
YousefProject <93458215+YousefProject@users.noreply.github.com>
2022-11-25 22:44:26 -0600

    firs commit
    intenti1.0

---
+++ resources/views/secciones/galeria.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <h2>Galeria</h2>
+</div>
+</section>
+<section class="gallery-images-section" itemprop="image" itemscope itemtype=" http://schema.org/ImageGallery">
+<div class="gallery-img-wrap">
+  <a href="img/2.jpeg" data-lightbox="example-set" data-title="Click the right half of the image to move forward.">
+    <img src="img/2.jpeg" alt="gallery-images">
+  </a>
+</div>
+<div class="gallery-img-wrap">
+  <a href="img/3.jpeg" data-lightbox="example-set" data-title="Click the right half of the image to move forward.">
+    <img src="img/3.jpeg" alt="gallery-images">
+  </a>
+</div>
+<div class="gallery-img-wrap">
+  <a href="img/4.jpeg" data-lightbox="example-set" data-title="Click the right half of the image to move forward.">
+    <img src="img/4.jpeg" alt="gallery-images">
+  </a>
+</div>
+<div class="gallery-img-wrap">
+  <a href="img/5.jpeg" data-lightbox="example-set" data-title="Click the right half of the image to move forward.">
+    <img src="img/5.jpeg" alt="gallery-images">
+  </a>
+</div>
+<div class="gallery-img-wrap">
+  <a href="img/6.jpeg" data-lightbox="example-set" data-title="Click the right half of the image to move forward.">
+    <img src="img/6.jpeg" alt="gallery-images">
+  </a>
+</div>
+<div class="gallery-img-wrap">
+  <a href="img/7.jpeg" data-lightbox="example-set" data-title="Click the right half of the image to move forward.">
+    <img src="img/7.jpeg" alt="gallery-images">
+  </a>
+</div>
+<div class="gallery-img-wrap">
+  <a href="img/8.jpeg" data-lightbox="example-set" data-title="Click the right half of the image to move forward.">
+    <img src="img/8.jpeg" alt="gallery-images">
+  </a>
+</div>
+
+<div class="gallery-img-wrap">
+  <a href="img/uno.jpeg" data-lightbox="example-set" data-title="Click the right half of the image to move forward.">
+    <img src="img/uno.jpeg" alt="gallery-images">
+  </a>
+</div>
+
+</section>
+@endsection
