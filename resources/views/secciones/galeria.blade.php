019cab09775fe9978b8179fe77514ddf087270ce
YousefProject <93458215+YousefProject@users.noreply.github.com>
2022-11-26 03:07:47 -0600

    all
    all

---
+++ resources/views/secciones/galeria.blade.php
@@ -0,0 +1,112 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="lugar"> 
+  <h1>
+  Parque Central Leon Nicaragua
+ </h1>
+ </div>
+ 
+ <div class="grid-gallery">
+     <a class="grid-gallery__item" href="#">
+         <img class="grid-gallery__image" src="img/2.jpeg">
+     </a>
+     <a class="grid-gallery__item" href="#">
+         <img class="grid-gallery__image" src="img/3.jpeg">
+     </a>
+ 
+  <a class="grid-gallery__item" href="#">
+         <img class="grid-gallery__image" src="img/4.jpeg">
+     </a>
+  
+  <a class="grid-gallery__item" href="#">
+         <img class="grid-gallery__image" src="img/5.jpeg">
+     </a>
+  
+  <a class="grid-gallery__item" href="#">
+         <img class="grid-gallery__image" src="img/6.jpeg">
+     </a>
+  
+     </a>
+  <a class="grid-gallery__item" href="#">
+         <img class="grid-gallery__image" src="img/7.jpeg">
+   
+ 
+     </a>
+ 
+  
+  
+     
+ </div>
+ 
+ 
+ 
+ <div class="map">
+ <iframe width="500" height="200" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="//www.google.com/maps/embed/v1/streetview?key=AIzaSyBuh6EOyKaaRSjAAzdPDn8TjcSbADwviDo&amp;location=12.4349246,-86.8787808&amp;fov=75" id="embed" allowfullscreen=""></iframe>
+ </div>
+ <div class="texto">
+  <div class="t1"></div>
+  <p>
+  En 1610, la Plaza Mayor de León se convirtió en el núcleo fundacional de la ciudad en su nuevo asentamiento.<br>
+  Su esquema corresponde a las Ordenanzas de Nueva Población dictadas en 1573 por Felipe II. <br>
+  Se caracteriza por su diversidad arquitectónica y una belleza excepcional. <br>
+      en la plaza se encuentran la propia Catedral de León, la Alcaldía Municipal de León, el Colegio y Capilla La Asunción (antiguo Palacio Episcopal) y el Antiguo Palacio Departamental (hoy Museo de la Revolución).
+  </p>
+ </div>
+ 
+ 
+ 
+ 
+ <div class="v2">
+ <div class="lugar"> 
+  <h1>
+  Centro de Convenciones OLOF PALME
+ </h1>
+ </div>
+ 
+ <div class="grid-gallery">
+     <a class="grid-gallery__item" href="#">
+         <img class="grid-gallery__image" src="galeria/a1.jpg">
+     </a>
+     <a class="grid-gallery__item" href="#">
+         <img class="grid-gallery__image" src="galeria/a2.jpg">
+     </a>
+ 
+  <a class="grid-gallery__item" href="#">
+         <img class="grid-gallery__image" src="galeria/a3.jpg">
+     </a>
+  
+  <a class="grid-gallery__item" href="#">
+         <img class="grid-gallery__image" src="galeria/a4.jpg">
+     </a>
+  
+  <a class="grid-gallery__item" href="#">
+         <img class="grid-gallery__image" src="galeria/a5.jpg">
+     </a>
+  
+     </a>
+  
+ 
+  
+  
+     
+ </div>
+ 
+ 
+ 
+ <div class="map2">
+ <iframe width="500" height="200" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="//www.google.com/maps/embed/v1/streetview?key=AIzaSyBuh6EOyKaaRSjAAzdPDn8TjcSbADwviDo&amp;location=12.1446119,-86.273465&amp;fov=75" id="embed" allowfullscreen=""></iframe>
+ </div>
+ <div class="texto">
+  <div class="t1"></div>
+  <p>
+  En 1610, la Plaza Mayor de León se convirtió en el núcleo fundacional de la ciudad en su nuevo asentamiento.<br>
+  Su esquema corresponde a las Ordenanzas de Nueva Población dictadas en 1573 por Felipe II. <br>
+  Se caracteriza por su diversidad arquitectónica y una belleza excepcional. <br>
+      en la plaza se encuentran la propia Catedral de León, la Alcaldía Municipal de León, el Colegio y Capilla La Asunción (antiguo Palacio Episcopal) y el Antiguo Palacio Departamental (hoy Museo de la Revolución).
+  </p>
+ </div>
+ 
+ 
+ </div>
+@endsection
